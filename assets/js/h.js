7f93fc7a341dbf6bc1c7585b4182a778f5da7a8d
Eugene Jeon <eugenejeon.me@gmail.com>
2022-04-05 13:52:45 +0900

    eslint 옵션 추가

--- assets/js/h.js
+++ assets/js/h.js
@@ -6,21 +6,22 @@ module.exports = class {
    * @param {string} context
    * @param {string} cls
    */
-  static sticky (target, context, cls) {
-    const $target = $(target)
-    const $context = $(context)
+  static sticky(target, context, cls) {
+    const $target = $(target);
+    const $context = $(context);
 
     if ($context.length) {
-      const contextOffsetTop = $context.offset().top
+      const contextOffsetTop = $context.offset().top;
 
-      return () => window.pageYOffset >= contextOffsetTop
-        ? $target.addClass(cls)
-        : $target.removeClass(cls)
+      return () =>
+        window.pageYOffset >= contextOffsetTop
+          ? $target.addClass(cls)
+          : $target.removeClass(cls);
     }
 
-    $target.addClass(cls)
+    $target.addClass(cls);
 
-    return () => {}
+    return () => {};
   }
 
   /**
@@ -28,11 +29,11 @@ module.exports = class {
    *
    * @param {string} indicator
    */
-  static scrollIndicator (indicator) {
-    const winScroll = $(window).scrollTop()
-    const height = $(document).outerHeight() - $(window).height()
+  static scrollIndicator(indicator) {
+    const winScroll = $(window).scrollTop();
+    const height = $(document).outerHeight() - $(window).height();
 
-    $(indicator).css('width', (winScroll / height) * 100 + '%')
+    $(indicator).css("width", (winScroll / height) * 100 + "%");
   }
 
   /**
@@ -43,38 +44,38 @@ module.exports = class {
    * @param {string} mode
    * @param {object} options
    */
-  static styledHeading (container, img, mode, options = {}) {
-    const $container = $(container)
+  static styledHeading(container, img, mode, options = {}) {
+    const $container = $(container);
 
     options = {
       controllable: options.controllable || false,
-      modeHandsControlElement: options.modeHandsControlElement || '',
-      attr: options.attr || 'data-mode'
-    }
+      modeHandsControlElement: options.modeHandsControlElement || "",
+      attr: options.attr || "data-mode",
+    };
 
-    const supports = ['default', 'tape', 'screen']
+    const supports = ["default", "tape", "screen"];
 
     /*
-      * Mode to 'default' if not exist thumbnail
-      */
+     * Mode to 'default' if not exist thumbnail
+     */
     if ($container.find(img).length <= 0) {
-      mode = 'default'
+      mode = "default";
     }
 
     /*
-      * Mode hands control
-      */
+     * Mode hands control
+     */
     if (options.controllable) {
-      const $modeHansControl = $(options.modeHandsControlElement)
+      const $modeHansControl = $(options.modeHandsControlElement);
 
       if ($modeHansControl.length) {
-        mode = $modeHansControl.attr(options.attr)
-        mode = supports.includes(mode) ? mode : 'default'
-        $modeHansControl.empty()
+        mode = $modeHansControl.attr(options.attr);
+        mode = supports.includes(mode) ? mode : "default";
+        $modeHansControl.empty();
       }
     }
 
-    $container.attr(options.attr, mode)
+    $container.attr(options.attr, mode);
   }
 
   /**
@@ -84,17 +85,17 @@ module.exports = class {
    * @param {string} target
    * @param {string} attr
    */
-  static liststyle (container, target, attr = 'data-mode') {
-    const $container = $(container)
-    const $list = $container.find(target)
+  static liststyle(container, target, attr = "data-mode") {
+    const $container = $(container);
+    const $list = $container.find(target);
 
-    const liststyle = $container.attr(attr)
+    const liststyle = $container.attr(attr);
 
     switch (liststyle) {
-      case 'gallery':
-      case 'grid':
-        $list.attr('class', 'uk-child-width-1-2@s uk-child-width-1-3@m')
-        UIkit.grid(container.concat(target))
+      case "gallery":
+      case "grid":
+        $list.attr("class", "uk-child-width-1-2@s uk-child-width-1-3@m");
+        UIkit.grid(container.concat(target));
     }
   }
 
@@ -109,26 +110,38 @@ module.exports = class {
    *
    * @param {object} options
    */
-  static alignImageCenter (options) {
+  static alignImageCenter(options) {
     const alignImage = function (width) {
-      width = width > 1100 ? 1100 : width
-      $(this).css({ width, 'max-width': 'none', 'margin-left': `calc((100% - ${width}px) / 2)` })
-    }
+      width = width > 1100 ? 1100 : width;
+      $(this).css({
+        width,
+        "max-width": "none",
+        "margin-left": `calc((100% - ${width}px) / 2)`,
+      });
+    };
 
     if (options.imageSlideBlock) {
-      $('figure.imageslideblock.alignCenter').each(function () {
-        alignImage.call(this, $(this).find('.image-wrap.selected > img').data('origin-width'))
-      })
+      $("figure.imageslideblock.alignCenter").each(function () {
+        alignImage.call(
+          this,
+          $(this).find(".image-wrap.selected > img").data("origin-width")
+        );
+      });
     }
     if (options.imageBlock) {
-      $('figure.imageblock.alignCenter').each(function () {
-        alignImage.call(this, $(this).data('origin-width') || $(this).attr('width') || $(this).width())
-      })
+      $("figure.imageblock.alignCenter").each(function () {
+        alignImage.call(
+          this,
+          $(this).data("origin-width") ||
+            $(this).attr("width") ||
+            $(this).width()
+        );
+      });
     }
     if (options.imageGridBlock) {
-      $('figure.imagegridblock').each(function () {
-        alignImage.call(this, 1100)
-      })
+      $("figure.imagegridblock").each(function () {
+        alignImage.call(this, 1100);
+      });
     }
   }
 
@@ -138,16 +151,23 @@ module.exports = class {
    * @param {string} container
    * @param {string} headings
    */
-  static anchorHeadings (container, headings) {
-    $(container).find(headings).each(function () {
-      const $heading = $(this)
-      const anchor = encodeURIComponent($heading.text()).replace(/[0-9-_.!~*'()\s]/g, '-')
-      const $a = $('<a></a>').attr('href', '#' + anchor).text($heading.text())
-      const htmlContent = $heading.html()
+  static anchorHeadings(container, headings) {
+    $(container)
+      .find(headings)
+      .each(function () {
+        const $heading = $(this);
+        const anchor = encodeURIComponent($heading.text()).replace(
+          /[0-9-_.!~*'()\s]/g,
+          "-"
+        );
+        const $a = $("<a></a>")
+          .attr("href", "#" + anchor)
+          .text($heading.text());
+        const htmlContent = $heading.html();
 
-      $a.html(htmlContent)
-      $heading.attr('id', anchor).empty().append($a)
-    })
+        $a.html(htmlContent);
+        $heading.attr("id", anchor).empty().append($a);
+      });
   }
 
   /**
@@ -161,30 +181,37 @@ module.exports = class {
    *
    * @param {object} options
    */
-  static scrollspy (options) {
+  static scrollspy(options) {
     /**
      * Extract Headings from target
      *
      * @param {string} headings
      */
     const extractHeadings = ($context, headings) => {
-      return $context.find(headings).get().reduce((spies, heading) => {
-        const $heading = $(heading)
-        const anchor = $heading.find('a').attr('href')
-        spies.push({ href: anchor, label: $heading.text(), tag: $heading.prop('tagName').toLowerCase() })
-        return spies
-      }, [])
-    }
-    const $spy = $(options.spy)
-    const $context = $(options.context)
+      return $context
+        .find(headings)
+        .get()
+        .reduce((spies, heading) => {
+          const $heading = $(heading);
+          const anchor = $heading.find("a").attr("href");
+          spies.push({
+            href: anchor,
+            label: $heading.text(),
+            tag: $heading.prop("tagName").toLowerCase(),
+          });
+          return spies;
+        }, []);
+    };
+    const $spy = $(options.spy);
+    const $context = $(options.context);
 
-    extractHeadings($context, options.headings).forEach(spy => {
-      const $a = $('<a></a>')
+    extractHeadings($context, options.headings).forEach((spy) => {
+      const $a = $("<a></a>")
         .text(spy.label)
-        .attr({ href: spy.href, 'data-tag': spy.tag })
+        .attr({ href: spy.href, "data-tag": spy.tag });
 
-      $spy.append($('<li></li>').append($a))
-    })
+      $spy.append($("<li></li>").append($a));
+    });
   }
 
   /**
@@ -195,18 +222,18 @@ module.exports = class {
    * @param {string} pos
    * @param {number} timeout
    */
-  static notification (notify, img, pos, timeout) {
-    const $notify = $(notify)
+  static notification(notify, img, pos, timeout) {
+    const $notify = $(notify);
 
     if ($notify.length > 0) {
-      const $thumbnail = $notify.find(img)
+      const $thumbnail = $notify.find(img);
 
-      if ($thumbnail.attr('src').indexOf('img_relation.png') > 0) {
-        $thumbnail.remove()
+      if ($thumbnail.attr("src").indexOf("img_relation.png") > 0) {
+        $thumbnail.remove();
       }
 
-      UIkit.notification($notify.get(0).outerHTML, { pos, timeout })
-      $notify.remove()
+      UIkit.notification($notify.get(0).outerHTML, { pos, timeout });
+      $notify.remove();
     }
   }
 
@@ -215,61 +242,67 @@ module.exports = class {
    *
    * @param {Object} messages
    */
-  static tooltip (messages) {
-    const $layerTooltip = $('.layer_tooltip')
-    const $tooltip = $layerTooltip.children('.inner_layer_tooltip')
-    const $reaction = $('.container_postbtn .postbtn_like .wrap_btn[id^=reaction-]')
+  static tooltip(messages) {
+    const $layerTooltip = $(".layer_tooltip");
+    const $tooltip = $layerTooltip.children(".inner_layer_tooltip");
+    const $reaction = $(
+      ".container_postbtn .postbtn_like .wrap_btn[id^=reaction-]"
+    );
 
     if ($reaction.length) {
       setTimeout(() => {
-        $layerTooltip.children('.inner_layer_tooltip').text(messages.Request)
-        $layerTooltip.fadeIn(500)
-      }, 5000)
+        $layerTooltip.children(".inner_layer_tooltip").text(messages.Request);
+        $layerTooltip.fadeIn(500);
+      }, 5000);
 
-      setTimeout(() => $layerTooltip.fadeOut(500), 10000)
+      setTimeout(() => $layerTooltip.fadeOut(500), 10000);
 
       $reaction.click(function () {
-        const tooltip = $(this).find('.btn_post > .uoc-icon').hasClass('like_on')
+        const tooltip = $(this)
+          .find(".btn_post > .uoc-icon")
+          .hasClass("like_on")
           ? messages.Unheart
-          : messages.Heart
+          : messages.Heart;
 
-        $tooltip.text(tooltip)
+        $tooltip.text(tooltip);
 
-        $layerTooltip.fadeIn(500)
-        setTimeout(() => $layerTooltip.fadeOut(500), 3000)
-      })
+        $layerTooltip.fadeIn(500);
+        setTimeout(() => $layerTooltip.fadeOut(500), 3000);
+      });
     }
 
-    $('.btn_menu_toolbar.btn_subscription').click(function () {
-      const tooltip = $(this).hasClass('following')
+    $(".btn_menu_toolbar.btn_subscription").click(function () {
+      const tooltip = $(this).hasClass("following")
         ? messages.Unsubscribe
-        : messages.Subscribe
+        : messages.Subscribe;
 
-      $tooltip.text(tooltip)
-    })
+      $tooltip.text(tooltip);
+    });
 
-    $('#tistorySnsLayer .btn_mark[data-service=url]').click(() => {
-      $tooltip.text(messages.Url)
-    })
+    $("#tistorySnsLayer .btn_mark[data-service=url]").click(() => {
+      $tooltip.text(messages.Url);
+    });
   }
 
   /**
    * Set foldable category
    */
-  static foldableCategory () {
-    const $subCategory = $('.tt_category .sub_category_list')
+  static foldableCategory() {
+    const $subCategory = $(".tt_category .sub_category_list");
 
     if ($subCategory.length) {
-      $subCategory.prev('.link_item').append('<i class="fas fa-chevron-down"></i>')
-      $subCategory.parent().addClass('foldable')
+      $subCategory
+        .prev(".link_item")
+        .append('<i class="fas fa-chevron-down"></i>');
+      $subCategory.parent().addClass("foldable");
     }
 
-    UIkit.accordion('.tt_category .category_list', {
-      content: '> .sub_category_list',
-      targets: '> .foldable',
-      toggle: '> .link_item',
-      multiple: true
-    })
+    UIkit.accordion(".tt_category .category_list", {
+      content: "> .sub_category_list",
+      targets: "> .foldable",
+      toggle: "> .link_item",
+      multiple: true,
+    });
   }
 
   /**
@@ -278,11 +311,11 @@ module.exports = class {
    * @param {string} el
    * @param {string} attr
    */
-  static switchTheme (light, dark, el = 'html', attr = 'data-theme') {
-    const TTDARK = $(el).attr(attr) === dark ? 'Y' : 'N'
+  static switchTheme(light, dark, el = "html", attr = "data-theme") {
+    const TTDARK = $(el).attr(attr) === dark ? "Y" : "N";
 
-    localStorage.TTDARK = TTDARK === 'Y' ? 'N' : 'Y'
-    $('html').attr('data-theme', TTDARK === 'Y' ? light : dark)
+    localStorage.TTDARK = TTDARK === "Y" ? "N" : "Y";
+    $("html").attr("data-theme", TTDARK === "Y" ? light : dark);
   }
 
   /**
@@ -293,13 +326,13 @@ module.exports = class {
    * @param {string} el
    * @param {string} attr
    */
-  static hljs (light, dark, el = 'html', attr = 'data-theme') {
-    const hljs = $(el).attr(attr) === 'dark' ? dark : light
+  static hljs(light, dark, el = "html", attr = "data-theme") {
+    const hljs = $(el).attr(attr) === "dark" ? dark : light;
 
-    $('link#__hljs').attr(
-      'href',
+    $("link#__hljs").attr(
+      "href",
       `//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/${hljs}.min.css`
-    )
+    );
   }
 
   /**
@@ -307,9 +340,12 @@ module.exports = class {
    *
    * @param {string} selector
    */
-  static removeTextNode (selector) {
-    $(selector).contents().filter(function () {
-      return this.nodeType === 3
-    }).remove()
+  static removeTextNode(selector) {
+    $(selector)
+      .contents()
+      .filter(function () {
+        return this.nodeType === 3;
+      })
+      .remove();
   }
-}
+};
