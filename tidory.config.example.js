5dda1bbddc5bca5f61f5e56280a07592ab3dcaca
Eugene Jeon <eugenejeon.me@gmail.com>
2022-04-05 13:54:01 +0900

    tidory.config.example.js 추가

---
+++ tidory.config.example.js
@@ -0,0 +1,59 @@
+/**
+ * Tidory Configuration
+ * https://tidory.com/docs/configuration/
+ * > https://github.com/tistory-projects/tistory-skin-hELLO
+ */
+module.exports = {
+  ts_session: "",
+  url: "",
+
+  preview: {
+    /**
+     * homeType
+     *
+     * NONE
+     * COVER
+     */
+    homeType: "NONE",
+
+    /**
+     * Preview Mode
+     *
+     * index
+     * entry
+     * category
+     * tag,
+     * location
+     * media,
+     * guestbook
+     */
+    mode: "index",
+  },
+
+  alias: {
+    "@": "assets",
+    "~views": "views",
+  },
+
+  /**
+   * Webpack Configuration
+   *
+   * @param {object} webpackConfig
+   */
+  extends(webpackConfig) {
+    webpackConfig.module.rules = [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      ...webpackConfig.module.rules,
+    ];
+
+    webpackConfig.entry = {
+      vendor: "./assets/vendor.js",
+      ...webpackConfig.entry,
+    };
+  },
+};
